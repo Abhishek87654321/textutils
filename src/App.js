07d0b3e6655d84bcf4a923bcdfc0093ca26e3774
Abhishek Tanwar <abhishekgangdhari@gmail.com>
2022-03-27 23:14:02 +0530

    added toglemode functinality

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import Navbar from "./components/Navbar";
+import React , { useState } from 'react'
 import TextForm from './components/TextForm'
 
-//  let sum = (a, b) => {
-//   return a + b;
-// }
 function App() {
+  const [mode, setMode] = useState('light') //  whether dark mode is enabled or not
+  const toggleMode = () =>{
+    if(mode === 'light'){
+      setMode ('dark');
+    }else{
+      setMode ('light');
+    }
+  }
   return (
     <>
     
-   <Navbar title= 'Navbar' nativePlace = 'Home' main = 'Link'/>
-   {/* <div> the sum of  5 and 9 is : {sum(5,9)}</div> */}
+   <Navbar title= 'Navbar' nativePlace = 'Home' main = 'Link' mode = {mode} toggleMode= {toggleMode}/>
    <div className="container"><TextForm title = 'Conversion Form'/>
    </div>
     </>
